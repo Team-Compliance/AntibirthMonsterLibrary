e6343c4c94bd9142db1295177cbb0e5f6a1e8cdb
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-07-27 11:56:21 -0500

    Red TNT remake part 1

--- scripts/redTNT.lua
+++ scripts/redTNT.lua
@@ -1,68 +1,41 @@
 local mod = AntiMonsterLib
 local game = Game()
 
-local RedTNTRoomRecord = {}
+function mod:RedTNTCollision()
 
-
-
--- Get red TNT room record
-function mod:GetRedTNTs()
-    local room_index = Game():GetLevel():GetCurrentRoomIndex()
-	
-	-- Get red TNT spawns
-    if game:GetRoom():IsFirstVisit() then
-		local redtnt_table = {}
-		
-        for _,v in pairs(Isaac.GetRoomEntities()) do
-			if v.Type == EntityType.ENTITY_AML and v.Variant == AMLVariants.RED_TNT then
-				table.insert(redtnt_table, v.Position)
-			end
-        end
-		RedTNTRoomRecord[room_index] = redtnt_table
-		
-	-- Respawn red TNTs
-    else
-		if RedTNTRoomRecord[room_index] ~= nil then
-			for _,v in pairs(RedTNTRoomRecord[room_index]) do
-				Isaac.Spawn(EntityType.ENTITY_AML, AMLVariants.RED_TNT, 0, v, Vector.Zero, nil)
-			end
-		end
-    end
 end
-mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.GetRedTNTs)
+mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, mod.RedTNTCollision)
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, mod.RedTNTCollision)
 
--- Reset red TNT record upon new stage entry
-function mod:RedTNTClearRecord()
-	RedTNTRoomRecord = {}
-end
-mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.RedTNTClearRecord)
-
-
-
--- Don't take damage
-function mod:RedTNTDamage(entity, amount, dmg_flags)
-	if entity.Variant == AMLVariants.RED_TNT then
-		return false
-	end
-end
-mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod.RedTNTDamage, EntityType.ENTITY_AML)
-
--- Explode on collision
-function mod:RedTNTCollision(entity, collider, low)
-	if entity.Variant == AMLVariants.RED_TNT then
-		local room_index = game:GetLevel():GetCurrentRoomIndex()
-		
-		Isaac.Explode(entity.Position, entity, 100)
-		entity.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
-		entity:GetSprite():Play("Blown")
-		
-		if RedTNTRoomRecord[room_index] ~= nil then
-			for i,v in pairs(RedTNTRoomRecord[room_index]) do 
-				if v == entity.Position then
-					table.remove(RedTNTRoomRecord[room_index], i)
+function mod:RedTNTUpdate()
+	local room = game:GetRoom()
+	for ind = 1, room:GetGridSize() do
+		local gridEnt = room:GetGridEntity(ind)
+		if gridEnt then
+			local tnt = gridEnt:ToTNT()
+			if tnt then
+				if tnt:GetVariant() == AMLVariants.RED_TNT then
+					local sprite = tnt:GetSprite()
+					sprite:ReplaceSpritesheet(0, "gfx/grid/grid_redtnt.png")
+					sprite:LoadGraphics()
+					
+					if tnt.State >= 1 then
+						tnt.State = 4
+					end
 				end
 			end
 		end
-    end
+	end
 end
-mod:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, mod.RedTNTCollision, EntityType.ENTITY_AML)
+mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.RedTNTUpdate)
+
+-- Replaces the old entity based red tnt with the new grid
+function mod:ReplaceOldRedTNT(entity)
+	local room = game:GetRoom()
+	if entity.Type == EntityType.ENTITY_AML and entity.Variant == AMLVariants.RED_TNT then
+		local index = room:GetClampedGridIndex(entity.Position)
+		entity:Remove()
+		room:SpawnGridEntity(index, GridEntityType.GRID_TNT, AMLVariants.RED_TNT, entity.InitSeed, 0)
+	end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.ReplaceOldRedTNT)
