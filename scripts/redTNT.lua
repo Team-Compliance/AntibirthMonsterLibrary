8443bfa6e63a334adf6fe7a15b1dbcfe0de0b7ef
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-08-17 12:14:58 -0500

    Better Red TNT

--- scripts/redTNT.lua
+++ scripts/redTNT.lua
@@ -1,18 +1,11 @@
 local mod = AntiMonsterLib
 local game = Game()
 
--- Just check collision and damage.
+-- Just check collision.
 function mod:RedTNTCollision(tnt, collider, low)
 	if tnt.Variant == AMLVariants.RED_TNT then
-		if collider:ToPlayer() or collider:ToNPC() then
-			tnt.HitPoints = 1
-		end
+		tnt:Die()
+		return false
 	end
 end
 mod:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, mod.RedTNTCollision, EntityType.ENTITY_MOVABLE_TNT)
-function mod:RedTNTDamage(tnt, amount, flags, source, frames)
-	if tnt.Variant == AMLVariants.RED_TNT then
-		tnt.HitPoints = 1
-	end
-end
-mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod.RedTNTDamage, EntityType.ENTITY_MOVABLE_TNT)
