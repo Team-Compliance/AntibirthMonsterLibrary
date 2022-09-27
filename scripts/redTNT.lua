dda179c6ce67551808827d3bfc101b8711fdf800
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-09-27 15:41:58 -0500

    Fix enemies pathfinding through Red TNT

--- scripts/redTNT.lua
+++ scripts/redTNT.lua
@@ -9,3 +9,12 @@ function mod:RedTNTCollision(tnt, collider, low)
 	end
 end
 mod:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, mod.RedTNTCollision, EntityType.ENTITY_MOVABLE_TNT)
+
+-- Set the grid path at the location so entities don't pathfind through it
+function mod:RedTNTUpdate(tnt)
+	if tnt.Variant == AMLVariants.RED_TNT and tnt.HitPoints == 4 then
+		local room = game:GetRoom()
+		room:SetGridPath(room:GetGridIndex(tnt.Position), 900)
+	end
+end
+mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, mod.RedTNTUpdate, EntityType.ENTITY_MOVABLE_TNT)
